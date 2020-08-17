45bdbaddde3d5430db23992ac0408d3bb8d84443
Avaer Kazmer <a@modules.io>
2020-08-17 08:35:09 -0400

    Add new methods to objectize.cc

--- objectize.cc
+++ objectize.cc
@@ -12,6 +12,10 @@ EMSCRIPTEN_KEEPALIVE void loadBake(GeometrySet *geometrySet, unsigned char *data
   doLoadBake(geometrySet, data, size);
 }
 
+EMSCRIPTEN_KEEPALIVE void getAnimalGeometry(GeometrySet *geometrySet, unsigned int hash, float **positions, float **colors, float **heads, float **legs, unsigned int **indices, unsigned int *numPositions, unsigned int *numColors, unsigned int *numIndices, unsigned int *numHeads, unsigned int *numLegs, float *headPivot, float *aabb) {
+  doGetAnimalGeometry(geometrySet, hash, positions, colors, heads, legs, indices, *numPositions, *numColors, *numIndices, *numHeads, *numLegs, headPivot, aabb);
+}
+
 EMSCRIPTEN_KEEPALIVE void doFree(void *ptr) {
   free(ptr);
 }
