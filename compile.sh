fc433313c52ba2710b3dfb68d4b31cfcbab12988
Avaer Kazmer <a@modules.io>
2020-08-18 11:13:24 -0400

    Major decoding integration work

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
 mkdir -p bin
-emcc --std=c++11 -s WASM=1 -s USE_PTHREADS=1 -O3 -s TOTAL_MEMORY=52428800 \
+emcc -s WASM=1 -s USE_PTHREADS=1 -O3 -s TOTAL_MEMORY=52428800 \
+  -Wno-almost-asm \
+  --memory-init-file 0 \
+  -fno-omit-frame-pointer \
+  --llvm-lto 1 \
+  -s NO_FILESYSTEM=1 \
+  -s PRECISE_F32=1 \
   objectize.cc vector.cc \
+  \
   draco/core/draco_types.cc \
 	draco/core/options.cc \
 	draco/core/data_buffer.cc \
@@ -33,6 +40,29 @@ emcc --std=c++11 -s WASM=1 -s USE_PTHREADS=1 -O3 -s TOTAL_MEMORY=52428800 \
 	draco/compression/attributes/sequential_attribute_decoders_controller.cc \
 	draco/compression/attributes/sequential_integer_attribute_decoder.cc \
 	draco/compression/attributes/sequential_quantization_attribute_decoder.cc \
+	\
+	draco/compression/encode.cc \
+	draco/mesh/mesh_misc_functions.cc \
+	draco/compression/mesh/mesh_encoder.cc \
+	draco/core/encoder_buffer.cc \
+	draco/mesh/triangle_soup_mesh_builder.cc \
+	draco/compression/expert_encode.cc \
+	draco/compression/point_cloud/point_cloud_encoder.cc \
+	draco/compression/mesh/mesh_edgebreaker_encoder.cc \
+	draco/compression/mesh/mesh_edgebreaker_encoder_impl.cc \
+	draco/compression/mesh/mesh_sequential_encoder.cc \
+	draco/compression/bit_coders/rans_bit_encoder.cc \
+	draco/metadata/metadata_encoder.cc \
+	draco/compression/attributes/attributes_encoder.cc \
+	draco/compression/attributes/sequential_attribute_encoder.cc \
+	draco/compression/attributes/sequential_attribute_encoders_controller.cc \
+	draco/compression/attributes/sequential_quantization_attribute_encoder.cc \
+	draco/compression/attributes/sequential_integer_attribute_encoder.cc \
+	draco/compression/entropy/shannon_entropy.cc \
+	draco/compression/entropy/symbol_encoding.cc \
+	draco/compression/attributes/prediction_schemes/prediction_scheme_encoder_factory.cc \
+	-DDRACO_ATTRIBUTE_VALUES_DEDUPLICATION_SUPPORTED=1 \
+	-DDRACO_ATTRIBUTE_INDICES_DEDUPLICATION_SUPPORTED=1 \
   -I. \
   --pre-js prefix.js --post-js postfix.js \
   -o bin/geometry.js
