0138f5284974e8f16d73db4e97c5dfa006597369
Bojan Škaljac <skaljac@gmail.com>
2021-11-27 14:07:38 +0100

    Pointer increment fixed

--- objectize.cc
+++ objectize.cc
@@ -51,8 +51,8 @@ EMSCRIPTEN_KEEPALIVE void raycastPhysicsArray(unsigned int rayCount, PScene *sce
     distance += 1;
     objectId += 1;
     faceIndex += 1;
-    outPosition += 3;
-    outQuaternion += 4;
+    outPosition += 1;
+    outQuaternion += 1;
   }
 }
 
