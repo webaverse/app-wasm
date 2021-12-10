6b65b72f74a2e09d473386214f20ea556c0deb59
Avaer Kazmer <a@modules.io>
2021-12-10 16:35:02 -0500

    Comment out moveCharacterControllerPhysics

--- objectize.cc
+++ objectize.cc
@@ -113,9 +113,9 @@ EMSCRIPTEN_KEEPALIVE void setVelocityPhysics(PScene *scene, unsigned int id, flo
 EMSCRIPTEN_KEEPALIVE PxController *createCharacterControllerPhysics(PScene *scene, float radius, float height, float *mat) {
   return scene->createCharacterController(radius, height, mat);
 }
-EMSCRIPTEN_KEEPALIVE unsigned int moveCharacterControllerPhysics(PScene *scene, PxController *characterController, float *displacement, float minDist, float elapsedTime, float *positionOut) {
+/* EMSCRIPTEN_KEEPALIVE unsigned int moveCharacterControllerPhysics(PScene *scene, PxController *characterController, float *displacement, float minDist, float elapsedTime, float *positionOut) {
   return scene->moveCharacterController(characterController, displacement, minDist, elapsedTime, positionOut);
-}
+} */
 
 EMSCRIPTEN_KEEPALIVE void setAngularLockFlagsPhysics(PScene *scene, unsigned int id, bool x, bool y, bool z) {
   scene->setAngularLockFlags(id, x, y, z);
