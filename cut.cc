082d14a41e6526e4120ca37fc1f494f24e5032b6
gonnavis <gonnavis@163.com>
2022-01-12 19:42:43 +0800

    fixed output lenght same as input length.

--- cut.cc
+++ cut.cc
@@ -13,7 +13,7 @@ void cut(
   float *position,
   float *quaternion,
   float *scale,
-  
+
   float *outPositions,
   unsigned int *numOutPositions,
   float *outNormals,
@@ -23,46 +23,19 @@ void cut(
 ) {
 
   {
-    // numOutPositions[0] = numPositions;
-    // for(int i=0;i<numPositions;i++){
-    //   outPositions[i] = positions[i];
-    // }
-    // numOutNormals[0] = numNormals;
-    // for(int i=0;i<numNormals;i++){
-    //   outNormals[i] = normals[i];
-    // }
+    numOutPositions[0] = numPositions;
+    for(int i=0;i<numPositions;i++){
+      outPositions[i] = positions[i];
+    }
+    numOutNormals[0] = numNormals;
+    for(int i=0;i<numNormals;i++){
+      outNormals[i] = normals[i];
+    }
 
-    // numOutFaces[0] = numFaces;
-    // for(int i=0;i<numFaces;i++){
-    //   outFaces[i] = faces[i];
-    // }
-    
-    numOutPositions[0] = 9;
-    outPositions[0] = positions[0];
-    outPositions[1] = positions[1];
-    outPositions[2] = positions[2];
-    outPositions[3] = positions[3];
-    outPositions[4] = positions[4];
-    outPositions[5] = positions[5];
-    outPositions[6] = positions[6];
-    outPositions[7] = positions[7];
-    outPositions[8] = positions[8];
-
-    numOutNormals[0] = 9;
-    outNormals[0] = normals[0];
-    outNormals[1] = normals[1];
-    outNormals[2] = normals[2];
-    outNormals[3] = normals[3];
-    outNormals[4] = normals[4];
-    outNormals[5] = normals[5];
-    outNormals[6] = normals[6];
-    outNormals[7] = normals[7];
-    outNormals[8] = normals[8];
-
-    numOutFaces[0] = 3;
-    outFaces[0] = faces[0];
-    outFaces[1] = faces[1];
-    outFaces[2] = faces[2];
+    numOutFaces[0] = numFaces;
+    for(int i=0;i<numFaces;i++){
+      outFaces[i] = faces[i];
+    }
   }
   {
     numOutPositions[1] = 0;
