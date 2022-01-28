f25a43041cf5a269aaa8511d62250d9590a8184f
Koreex Jin <koreex.jin@gmail.com>
2022-01-28 22:13:45 +0800

    remove unused codes

--- march.h
+++ march.h
@@ -1,17 +1,6 @@
 #ifndef MARCH_H
 #define MARCH_H
 
-#include "vector.h"
-#include <cstdlib>
-#include <limits>
-#include <array>
-#include <deque>
-#include <map>
-#include <set>
-
-void smoothedPotentials(int *chunkCoords, unsigned int numChunkCoords, float *colorTargetCoordBuf, int colorTargetSize, float voxelSize, float *potentialsBuffer);
 float* marchingCubes(int dims[3], float *potential, float shift[3], float scale[3]);
-void computeGeometry(int *chunkCoords, unsigned int numChunkCoords, float *colorTargetCoordBuf, int colorTargetSize, float voxelSize, float marchCubesTexSize, float marchCubesTexSquares, float marchCubesTexTriangleSize, float *potentialsBuffer, float *positions, float *barycentrics, float *uvs, float *uvs2, unsigned int *positionIndex, unsigned int *barycentricIndex, unsigned int *uvIndex, unsigned int *uvIndex2);
-void collide(float *positions, unsigned int numPositions, float origin[3], float direction[3], float *collision, unsigned int *collisionIndex);
 
 #endif
