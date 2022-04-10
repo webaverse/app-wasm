9a4a4ef32a6fde5a7bba69a5159a823104c9f7a8
Avaer Kazmer <a@modules.io>
2022-04-09 21:06:03 -0400

    Return uint8_t * from marchingCubes

--- march.h
+++ march.h
@@ -1,6 +1,8 @@
 #ifndef MARCH_H
 #define MARCH_H
 
-float* marchingCubes(int dims[3], float *potential, float shift[3], float scale[3]);
+#include <cstdint>
+
+uint8_t* marchingCubes(int dims[3], float *potential, float shift[3], float scale[3]);
 
 #endif
