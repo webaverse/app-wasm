ce2afc61535ce0ceee8e5b7a69697ba5ac2ad3a3
Avaer Kazmer <a@modules.io>
2022-05-28 22:32:55 -0400

    Add chunkDamageBuffer.cc

---
+++ DualContouring/chunkDamageBuffer.cc
@@ -0,0 +1,66 @@
+#include "chunkDamageBuffer.h"
+
+/* ChunkDamageBuffer(const ChunkDamageBuffer &other) {
+  min = other.min;
+  gridPoints = other.gridPoints;
+  cachedDamageField = other.cachedDamageField;
+} */
+ChunkDamageBuffer::ChunkDamageBuffer(const ChunkDamageBuffer &&other) {
+  min = other.min;
+  size = other.size;
+  gridPoints = other.gridPoints;
+  cachedDamageField = std::move(other.cachedDamageField);
+}
+ChunkDamageBuffer::ChunkDamageBuffer(const vm::ivec3 &min, const int &size) :
+  min(min),
+  size(size),
+  gridPoints(size + 3),
+  cachedDamageField(gridPoints * gridPoints)
+  {}
+/* ChunkDamageBuffer &ChunkDamageBuffer::operator=(const ChunkDamageBuffer &other) {
+  min = other.min;
+  gridPoints = other.gridPoints;
+  cachedDamageField = other.cachedDamageField;
+  return *this;
+} */
+ChunkDamageBuffer &ChunkDamageBuffer::operator=(ChunkDamageBuffer &&other) {
+  min = other.min;
+  size = other.size;
+  gridPoints = other.gridPoints;
+  cachedDamageField = std::move(other.cachedDamageField);
+  return *this;
+}
+
+//
+
+bool ChunkDamageBuffer::drawDamage(const float &x, const float &y, const float &z, const float radius, const float value) {
+  int ax = int(x) - min.x + 1;
+  int az = int(z) - min.z + 1;
+  bool drew = false;
+  for (float dx = -radius; dx <= radius; dx++) {
+    for (float dz = -radius; dz <= radius; dz++) {
+      for (float dy = -radius; dy <= radius; dy++) {
+        int lx = ax + dx;
+        int lz = az + dz;
+        if (lx >= 0 && lx < gridPoints && lz >= 0 && lz < gridPoints) {
+          float distance = sqrt(dx * dx + dy * dy + dz * dz);
+          float distanceFactor = 1 - distance / radius;
+          int index = lx + lz * gridPoints;
+          // if (index >= 0 && index < cachedDamageField.size()) {
+            cachedDamageField[index] += value * distanceFactor;
+            drew = true;
+          /* } else {
+            std::cout << "invalid draw damage" << std::endl;
+          } */
+        }
+      }
+    }
+  }
+  return drew;
+}
+
+float ChunkDamageBuffer::getInterpolated(const float &x, const float &z) {
+  const float localX = x - min.x + 1;
+  const float localZ = z - min.z + 1;
+  return interpolate2D(localX, localZ);
+}
