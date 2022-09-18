b0d7ca69948dcec53777e2787bba3ab772c85c05
Avaer Kazmer <a@modules.io>
2022-09-18 11:03:54 -0400

    Plug in new meshoptimizer apis

--- worker.cc
+++ worker.cc
@@ -5,6 +5,7 @@
 // #include "march.h"
 // #include "collide.h"
 #include "physics.h"
+#include "meshoptimizer/meshoptimizer.h"
 // #include "convex.h"
 // #include "earcut.h"
 // #include <iostream>
@@ -45,6 +46,54 @@ EMSCRIPTEN_KEEPALIVE void cookConvexGeometryPhysics(float *positions, unsigned i
 
 EMSCRIPTEN_KEEPALIVE void deleteMemoryOutputStream(PxDefaultMemoryOutputStream *writeStream) {
   delete writeStream;
+}
+
+//
+
+// MESHOPTIMIZER_API size_t meshopt_simplify(unsigned int* destination, const unsigned int* indices, size_t index_count, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride, size_t target_index_count, float target_error, unsigned int options, float* result_error);
+EMSCRIPTEN_KEEPALIVE void meshoptSimplify(
+  const unsigned int* indices,
+  size_t index_count,
+  const float* vertex_positions,
+  size_t vertex_count,
+  size_t target_index_count,
+  float target_error,
+  unsigned int **destination,
+  unsigned int *numDestinations
+) {
+  *destination = (unsigned int *)malloc(sizeof(unsigned int) * index_count);
+  constexpr size_t vertex_positions_stride = 3 * sizeof(float);
+  constexpr unsigned int options = 0;
+  float result_error;
+  *numDestinations = meshopt_simplify(*destination, indices, index_count, vertex_positions, vertex_count, vertex_positions_stride, target_index_count, target_error, options, &result_error);
+  std::cout << "simplify wasm " << *destination << " " << *numDestinations << std::endl;
+}
+
+// MESHOPTIMIZER_EXPERIMENTAL size_t meshopt_simplifySloppy(unsigned int* destination, const unsigned int* indices, size_t index_count, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride, size_t target_index_count, float target_error, float* result_error);
+
+// indices.byteOffset,
+// indices.length,
+// positions.byteOffset,
+// positions.length,
+// target_index_count,
+// scratchStack.u32.byteOffset,
+// scratchStack.u32.byteOffset + Uint32Array.BYTES_PER_ELEMENT
+
+EMSCRIPTEN_KEEPALIVE void meshoptSimplifySloppy(
+  const unsigned int* indices,
+  size_t index_count,
+  const float* vertex_positions,
+  size_t vertex_count,
+  size_t target_index_count,
+  float target_error,
+  unsigned int **destination,
+  unsigned int *numDestinations
+) {
+  *destination = (unsigned int *)malloc(sizeof(unsigned int) * index_count);
+  constexpr size_t vertex_positions_stride = 3 * sizeof(float);
+  float result_error;
+  *numDestinations = meshopt_simplifySloppy(*destination, indices, index_count, vertex_positions, vertex_count, vertex_positions_stride, target_index_count, target_error, &result_error);
+  std::cout << "simplify sloppy wasm " << *destination << " " << *numDestinations << std::endl;
 }
 
 } // extern "C"
