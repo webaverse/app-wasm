29ca9a1b84438e6444e6f7bd93c0527f46895415
gonnavis <gonnavis@163.com>
2022-09-26 05:14:59 +0800

    Add `constants.h` and move `BoneIndex` into it.

--- AnimationSystem/AnimationSystem.h
+++ AnimationSystem/AnimationSystem.h
@@ -1,6 +1,7 @@
 #ifndef _ANIMATIONSYSTEM_H
 #define _ANIMATIONSYSTEM_H
 #include "physics.h"
+#include "constants.h"
 #include <iostream>
 
 namespace AnimationSystem {
@@ -10,62 +11,6 @@ namespace AnimationSystem {
   class Avatar;
   class AnimationNode;
   class AnimationMixer;
-  
-  enum BoneIndex {
-    // Hips = 0, // position
-    Hips = 1, // quaternion
-    Spine = 2,
-    Chest = 3,
-    UpperChest = 4,
-    Neck = 5,
-    Head = 6,
-    Left_shoulder = 7,
-    Left_arm = 8,
-    Left_elbow = 9,
-    Left_wrist = 10,
-    Left_middleFinger1 = 11,
-    Left_middleFinger2 = 12,
-    Left_middleFinger3 = 13,
-    Left_thumb0 = 14,
-    Left_thumb1 = 15,
-    Left_thumb2 = 16,
-    Left_indexFinger1 = 17,
-    Left_indexFinger2 = 18,
-    Left_indexFinger3 = 19,
-    Left_ringFinger1 = 20,
-    Left_ringFinger2 = 21,
-    Left_ringFinger3 = 22,
-    Left_littleFinger1 = 23,
-    Left_littleFinger2 = 24,
-    Left_littleFinger3 = 25,
-    Right_shoulder = 26,
-    Right_arm = 27,
-    Right_elbow = 28,
-    Right_wrist = 29,
-    Right_middleFinger1 = 30,
-    Right_middleFinger2 = 31,
-    Right_middleFinger3 = 32,
-    Right_thumb0 = 33,
-    Right_thumb1 = 34,
-    Right_thumb2 = 35,
-    Right_indexFinger1 = 36,
-    Right_indexFinger2 = 37,
-    Right_indexFinger3 = 38,
-    Right_ringFinger1 = 39,
-    Right_ringFinger2 = 40,
-    Right_ringFinger3 = 41,
-    Right_littleFinger1 = 42,
-    Right_littleFinger2 = 43,
-    Right_littleFinger3 = 44,
-    Right_leg = 45,
-    Right_knee = 46,
-    Right_ankle = 47,
-    Right_toe = 48,
-    Left_leg = 49,
-    Left_knee = 50,
-    Left_ankle = 51,
-    Left_toe = 52
-  };
 
   struct Interpolant {
     unsigned int numParameterPositions;
